47c205d7a5b2be3878ce4b16628056a0661d37e0
INJAMAM <injamamul937@gmail.com>
2025-01-31 23:12:26 +0600

    done

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import bodyParser from "body-parser";
+// import { render } from "ejs";
+
+const app = express();
+const port = 5000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.post("/submit", (req, res) => {
+  const data = {
+    header: "Enter your name below:",
+    body: req.body,
+    length: req.body.fName.length + req.body.lName.length,
+  };
+  res.render("index.ejs", data);
+  console.log(req.body);
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
